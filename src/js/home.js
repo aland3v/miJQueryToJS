0541ce0997ca3f94f9728d37be84a120638ea097
Alan dev <aland3v@gmail.com>
2021-01-09 14:45:04 -0400

    Promesas

--- src/js/home.js
+++ src/js/home.js
@@ -1,23 +1,40 @@
-console.log('hola mundo!');
-const noCambia = "Leonidas";
+console.log('Hola Alan!');
+const noCambia = "Alan";
 
-let cambia = "@LeonidasEsteban"
+let cambia = "@pydv77"
 
 function cambiarNombre(nuevoNombre) {
   cambia = nuevoNombre
 }
 
-
-const getUser = new Promise(function(todoBien, todoMal) {
-  setTimeout(function(){
-    todoMal('Se acabó el tiempo');
-  }, 3000);
+const getUserAll = new Promise((todoBien, todoMal) => {
+  setTimeout(() =>
+    true ? todoBien("Todo bien getAll") : todoMal("Todo mal getAll"), 5000)
 });
 
-getUser
-  .then(function() {
-    console.log("todo esta bien en la vida");
-  })
-  .catch(function(message) {
-    console.log(message);
-  })
+const getUser = new Promise((todoBien, todoMal) => {
+  setTimeout(() =>
+  true ? todoBien("Todo bien get") : todoMal("Todo mal get"), 6000);
+});
+
+// getUser
+//   .then(function() {
+//     console.log("todo esta bien en la vida");
+//   })
+//   .catch(function(message) {
+//     console.log(message);
+//   })
+
+// Promise.race([
+//   getUser,
+//   getUserAll,
+// ])
+// .then(response => console.log(response))
+// .catch(message => console.log(message));
+
+Promise.all([
+  getUser,
+  getUserAll,
+])
+.then(response => console.log(response)) // devuelve un array con cada response
+.catch(message => console.log(message)); // cacha el 1er error encontrado
